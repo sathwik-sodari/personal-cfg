9d43006d3230c6a5ae8d8a89e49612227a807f14
Prachi <prachimangewala@gmail.com>
2021-07-10 13:15:33 +0530

    Backend changes

---
+++ server.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import userRouter from './Routers/userRouter.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+});
+
+app.get('/', (req, res) => {
+    res.send('Server is ready');
+});
+
+app.use('/api/users', userRouter);
+
+app.use((err, req, res, next) => {
+    res.status(500).send({message: err.message});
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log(`Serving at port ${port}`);
+});
